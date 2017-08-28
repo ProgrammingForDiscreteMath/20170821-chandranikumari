52d89955aa9f0b4837de9ed93b8c378210eca93b
chandranikumari <chandraniku@gmail.com>
2017-08-28 23:59:46 +0530

    Update code_assign1.py

--- code_assign1.py
+++ code_assign1.py
@@ -5,11 +5,10 @@
 L=[]
 for i in range(10):
     p=(i*(i+1))/2
-    int(p)
     L.append(p)
 
-print (L)
-
+#print (L)
+'''****************************************************************************'''
 # Create a function to test if a number is prime
 def is_prime(n):
     """
@@ -25,25 +24,30 @@ def is_prime(n):
     else:
         print ("False")
 
+#print (is_prime(17))
 # Tests
 # is_prime(2033) == False
 # is_prime(2039) == True
+'''**************************************************************************'''
+
 
 # Create a function which returns a list of the first ten prime numbers
 # greater than or equal to n
 
 def next_ten_primes(n):
-    """
-    Return the list of the first ten prime numbers greate than or equal to n
-    """      
+    prime1=[]
+    while (len(prime1)!=10):
+        for i in range(2,n):
+            if (n%i==0):
+                break
+            else:
+                prime1.append(n)
+                break
+        n=n+1
+    return prime1
 
+#print (next_ten_primes(75))
 
 # Tests
 # next_ten_primes(2033) == [2039, 2053, 2063, 2069, 2081, 2083, 2087, 2089, 2099, 2111]
 # next_ten_primes(2039) == [2039, 2053, 2063, 2069, 2081, 2083, 2087, 2089, 2099, 2111]
-
-
-
-
-
-
